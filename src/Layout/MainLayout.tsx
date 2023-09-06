879b2e3448c27e5e0c5de63c8e022a30b55561f9
Alejandro Toledo <jairoalejandro2003@gmail.com>
2023-09-06 07:30:54 -0500

    Some new meta tags

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -14,15 +14,20 @@ const origin=(typeof window==='undefined' ? '' : window.location.origin);
 const fullUrl=(typeof window==='undefined' ? '' : window.location);
 
 export const MainLayout:FC<props> = ({title,description,children}) => {
+
   return (
     <>
         <Head>
             <title>{title}</title>
             <link rel="icon" href={`${origin}/images/logo_portfolio.png`} />
-            <meta property='og:description' name='description' content={description}/>
+            <meta property='og:description' content={description}/>
             <meta property='og:title' content={title}/>
             <meta property='og:image' content={`${origin}/images/imagen_portfolio.png`}/>
             <meta property='og:url' content={`${fullUrl}`}/>
+            <meta property='twitter:description' content={description}/>
+            <meta property='twitter:title' content={title}/>
+            <meta property='twitter:image' content={`${origin}/images/imagen_portfolio.png`}/>
+            <meta property='twitter:url' content={`${fullUrl}`}/>
         </Head>
         <SideBar/>
         <NavBar/>
